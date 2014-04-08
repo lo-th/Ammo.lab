a4b0e60f4ead0c8f57334a7e9c95ae5f759d304e
lo-th <3dflashlo@gmail.com>
2014-04-08 03:56:26 +0200

    cone up

--- js/AMMO.Three.js
+++ js/AMMO.Three.js
@@ -200,7 +200,7 @@ AMMO.Rigid.prototype = {
 			break;
 			case 'sphere': shape = new Ammo.btSphereShape(size[0]); break;	
 			case 'cylinder': shape = new Ammo.btCylinderShape(AMMO.V3(size[0], size[1]*0.5, size[2]*0.5)); break;
-			case 'cone': shape = new Ammo.btConeShape(size[0]*0.5, size[1]*0.5); break;
+			case 'cone': shape = new Ammo.btConeShape(size[0], size[1]*0.5); break;
 			case 'capsule': shape = new Ammo.btCapsuleShape(size[0], size[1]*0.5); break;
 			case 'mesh': shape = new Ammo.btBoxShape(AMMO.V3(size[0]*0.5, size[1]*0.5, size[2]*0.5)); break;
 			case 'convex': shape = new Ammo.btBoxShape(AMMO.V3(size[0]*0.5, size[1]*0.5, size[2]*0.5)); break;
