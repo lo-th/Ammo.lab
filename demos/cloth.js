7d7bd28a9fb3b26029b41012886f3d1a3a2fd58a
lo-th <3dflashlo@gmail.com>
2015-12-18 18:10:01 +0100

    up

--- demos/cloth.js
+++ demos/cloth.js
@@ -5,13 +5,13 @@ function demo() {
     view.setShadowPosY(-10.1)
 
     // infinie plane
-    add({type:'plane', name:'ground', pos:[0,-10,0]});
+    //add({type:'plane', name:'ground', pos:[0,-10,0]});
 
     // static box
-    add({ type:'box', size:[10,10,4], pos:[0,-5,8.2], rot:[45,0,0], mass:0 });
-    add({ type:'box', size:[10,10,10], pos:[0,-9,0], rot:[0,0,0], mass:0 });
+    //add({ type:'box', size:[10,10,4], pos:[0,-5,8.2], rot:[45,0,0], mass:0 });
+    add({ type:'box', size:[10,10,10], pos:[0,-5,0], rot:[0,0,0], mass:0 });
     // box filter
-    add({ type:'box', size:[10,6,4], pos:[0,-5,-4.8], rot:[0,0,0], mass:0 });
+    //add({ type:'box', size:[10,6,4], pos:[0,-5,-4.8], rot:[0,0,0], mass:0 });
 
     add({ 
         type:'cloth', name:'cloth', 
@@ -26,9 +26,10 @@ function demo() {
         gendiags:true,
     });
 
-    add({ type:'sphere', size:[3], pos:[-20, 100, 0], mass:1})
-    add({ type:'sphere', size:[3], pos:[20, 200, 0], mass:1})
-    add({ type:'sphere', size:[3], pos:[0, 300, -20], mass:1})
+    add({ type:'sphere', size:[3], pos:[-20, 100, 0], mass:1});
+    add({ type:'sphere', size:[3], pos:[20, 200, 0], mass:1});
+    add({ type:'sphere', size:[3], pos:[0, 300, -20], mass:1});
+    add({ type:'sphere', size:[3], pos:[0, 400, 20], mass:1});
 
     //anchor({soft:'cloth', body:'ground', pos:0});
     //anchor({soft:'cloth', body:'ground', pos:100});
