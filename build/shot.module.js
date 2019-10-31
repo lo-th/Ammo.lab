29c3f45f420662720ae020ee9ad872b80a5e7780
lo-th <3dflashlo@gmail.com>
2019-10-31 17:03:13 +0100

    up

--- build/shot.module.js
+++ build/shot.module.js
@@ -1347,33 +1347,40 @@ function geometryInfo( g, type ) {
 * CAPSULE GEOMETRY
 */
 
-function Capsule( radius, height, radialSegments, heightSegments ) {
+function Capsule( radius, height, radialSegs, heightSegs ) {
 
 	THREE.BufferGeometry.call( this );
 
 	this.type = 'Capsule';
 
-	radius = radius || 1;
+    radius = radius || 1;
     height = height || 1;
 
-    radialSegments = Math.floor( radialSegments ) || 12;
-    var sHeight = Math.floor( radialSegments * 0.5 );
+    var pi = Math.PI;
 
-    heightSegments = Math.floor( heightSegments ) || 1;
+    radialSegs = Math.floor( radialSegs ) || 12;
+    var sHeight = Math.floor( radialSegs * 0.5 );
+
+    heightSegs = Math.floor( heightSegs ) || 1;
     var o0 = Math.PI * 2;
     var o1 = Math.PI * 0.5;
     var g = new THREE.Geometry();
-    var m0 = new THREE.CylinderGeometry( radius, radius, height, radialSegments, heightSegments, true );
-    var m1 = new THREE.SphereGeometry( radius, radialSegments, sHeight, 0, o0, 0, o1);
-    var m2 = new THREE.SphereGeometry( radius, radialSegments, sHeight, 0, o0, o1, o1);
+    var m0 = new THREE.CylinderGeometry( radius, radius, height, radialSegs, heightSegs, true );
+
+    var mr = new THREE.Matrix4();
+    var m1 = new THREE.SphereGeometry( radius, radialSegs, sHeight, 0, o0, 0, o1);
+    var m2 = new THREE.SphereGeometry( radius, radialSegs, sHeight, 0, o0, o1, o1);
     var mtx0 = new THREE.Matrix4().makeTranslation( 0,0,0 );
-    if(radialSegments===6) mtx0.makeRotationY(30*0.0174532925199432957);
+   // if(radialSegs===6) mtx0.makeRotationY( 30 * THREE.Math.DEG2RAD );
     var mtx1 = new THREE.Matrix4().makeTranslation(0, height*0.5,0);
     var mtx2 = new THREE.Matrix4().makeTranslation(0, -height*0.5,0);
-    g.merge( m0, mtx0);
+    mr.makeRotationZ( pi );
+    g.merge( m0, mtx0.multiply(mr) );
     g.merge( m1, mtx1);
     g.merge( m2, mtx2);
+
     g.mergeVertices();
+    g.computeVertexNormals();
 
     m0.dispose();
     m1.dispose();
